9758a2a6664d0fc04378bfd92b24276fe6853457
Ryan Machan <rjm21@case.edu>
2018-02-12 18:47:35 -0500

    add rating to useralbum model

--- models/useralbum.js
+++ models/useralbum.js
@@ -1,5 +1,7 @@
-module.exports = function(sequelize) {
+module.exports = function(sequelize, DataTypes) {
 	var UserAlbum = sequelize.define('UserAlbum', {
+		rating: { type: DataTypes.INTEGER, allowNull: true, validate: { len: [1] } }
+	}, {
 		timestamps: false,
 		freezeTableName: true
 	});
