1e48179150855652623e20c0dc1d5b47eb6e416a
Ryan Machan <rjm21@case.edu>
2018-02-14 18:32:07 -0500

    update user model so username has to be unique

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
 	var User = sequelize.define('User', {
-		username: { type: DataTypes.STRING, allowNull: false, validate: { len: [1,255] } },
+		username: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { len: [1,255] } },
 		password: { type: DataTypes.CHAR, allowNull: false, validate: { len: [1,60] } },
 		email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { len: [1,255] } },
 		name: { type: DataTypes.STRING, allowNull: true, validate: { len: [0,255] } },
