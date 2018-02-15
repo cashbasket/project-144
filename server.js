0152c1c583d6e1ecc3cf615467c83ee7ec6d7dea
Ryan Machan <rjm21@case.edu>
2018-02-14 21:10:34 -0500

    stub out initial server.js to test routes

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+var db = require('./models');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+global.__root   = __dirname + '/'; 
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+// Import controllers
+var RootController = require(__root + 'controllers/RootController');
+app.use(RootController);
+
+var UserController = require(__root + 'controllers/UserController');
+app.use('/api/user', UserController);
+
+var AlbumController = require(__root + 'controllers/AlbumController');
+app.use('/api/album', AlbumController);
+
+var PostController = require(__root + 'controllers/PostController');
+app.use('/api/post', PostController);
+
+// Sync with DB and then listen
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Listening on port %s', PORT);
+	});
+});
