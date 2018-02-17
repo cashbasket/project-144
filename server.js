e503aa4738cd176eb4799f3c3770b3414285533e
Ryan Machan <rjm21@case.edu>
2018-02-17 10:43:25 -0500

    fix server.js and include handlebars

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ require('dotenv').config();
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 var db = require('./models');
 
 var app = express();
 var RateLimit = require('express-rate-limit');
 var PORT = process.env.PORT || 3000;
+var limiter = new RateLimit({
+	windowMs: 15*60*1000,
+	max: 100,
+	delayMs: 0
+});
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(limiter);
 
 app.use(express.static('public'));
 
-app.use(limiter);
+// Set Handlebars as the default templating engine.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Import controllers
 var RootController = require('./controllers/RootController');
@@ -23,12 +32,6 @@ app.use(RootController);
 
 var ApiController = require('./controllers/ApiController');
 app.use('/api', ApiController);
-
-var limiter = new RateLimit({
-	windowMs: 15*60*1000, // 15 minutes
-	max: 100, // limit each IP to 100 requests per windowMs
-	delayMs: 0 // disable delaying - full speed until the max limit is reached
-  });
 
 // Sync with DB and then listen
 db.sequelize.sync().then(function() {
