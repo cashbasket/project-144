d2de640f6f556ccd8b5ee862f777ac3c9c6df5f7
Ryan Machan <rjm21@case.edu>
2018-02-28 23:22:20 -0500

    update user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+var bcrypt = require('bcrypt-nodejs');
 module.exports = function(sequelize, DataTypes) {
 	var User = sequelize.define('User', {
 		username: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { len: [1,255] } },
@@ -26,9 +27,9 @@ module.exports = function(sequelize, DataTypes) {
 		delete values.resetPasswordExpires;
 		return values;
 	};
-
-	User.prototype.formatCreateDate = function() {
-		return this.createdAt;
+	
+	User.prototype.validPassword = function(password) {
+		return bcrypt.compareSync(password, this.password);
 	};
 
 	return User;
